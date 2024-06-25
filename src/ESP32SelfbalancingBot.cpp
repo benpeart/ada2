765a8d1a01dc51e03481e96b3694d0e14e3d19ae
Ben Peart <benpeart@msn.com>
2024-06-25 11:38:49 -0400

    Update project so it can be built with platform.io Update pin numbers in defines.h to match my hardware setup

--- src/ESP32SelfbalancingBot.cpp
+++ src/ESP32SelfbalancingBot.cpp
@@ -1,3 +1,6 @@
+#define DEBUG_OSC
+#define DEBUG
+
 #include <Arduino.h>
 #include <WiFi.h>
 #include <WiFiClient.h>
@@ -17,25 +20,29 @@
 #include <WiFiUdp.h>
 #include "OSC.h"
 #include "esp32-hal-ledc.h"
+#include "debug.h"
 
 void initTimers();
 
-void initWifiAP() {
-	//Serial.println("Setting up WiFi AP...");
-	if (WiFi.softAP("bbot", "12345678")) {
-		Serial.println("Wifi AP set up successfully");
+void initWifiAP()
+{
+	// DB_PRINTLN("Setting up WiFi AP...");
+	if (WiFi.softAP("bbot", "12345678"))
+	{
+		DB_PRINTLN("Wifi AP set up successfully");
 	}
-	WiFi.softAPConfig(IPAddress(192, 168, 4, 1), IPAddress(192, 168, 4, 1),
-			IPAddress(255, 255, 255, 0));
+	WiFi.softAPConfig(IPAddress(192, 168, 4, 1), IPAddress(192, 168, 4, 1), IPAddress(255, 255, 255, 0));
 }
 
-void initMPU6050() {
+void initMPU6050()
+{
 	MPU6050_setup();
 	delay(500);
 	MPU6050_calibrate();
 }
 
-void setup() {
+void setup()
+{
 	pinMode(PIN_ENABLE_MOTORS, OUTPUT);
 	digitalWrite(PIN_ENABLE_MOTORS, HIGH);
 
@@ -46,8 +53,8 @@ void setup() {
 
 	pinMode(PIN_SERVO, OUTPUT);
 
-	ledcSetup(6, 50, 16); // channel 6, 50 Hz, 16-bit width
-	ledcAttachPin(PIN_SERVO, 6);   // GPIO 22 assigned to channel 1
+	ledcSetup(6, 50, 16);		 // channel 6, 50 Hz, 16-bit width
+	ledcAttachPin(PIN_SERVO, 6); // GPIO 22 assigned to channel 1
 	delay(50);
 	ledcWrite(6, SERVO_AUX_NEUTRO);
 
@@ -63,7 +70,8 @@ void setup() {
 	OSC_init();
 
 	digitalWrite(PIN_ENABLE_MOTORS, LOW);
-	for (uint8_t k = 0; k < 5; k++) {
+	for (uint8_t k = 0; k < 5; k++)
+	{
 		setMotorSpeedM1(5);
 		setMotorSpeedM2(5);
 		ledcWrite(6, SERVO_AUX_NEUTRO + 250);
@@ -78,63 +86,87 @@ void setup() {
 	digitalWrite(PIN_ENABLE_MOTORS, HIGH);
 }
 
-void processOSCMsg() {
-	if (OSCpage == 1) {
-		if (modifing_control_parameters)  // We came from the settings screen
+void processOSCMsg()
+{
+#ifdef DEBUG_OSC
+	DB_PRINTLN("processOSCMsg");
+#endif	
+	if (OSCpage == 1)
+	{
+		if (modifing_control_parameters) // We came from the settings screen
 		{
+			DB_PRINTLN("OSC modifing_control_parameters");
 			OSCfader[0] = 0.5; // default neutral values
 			OSCfader[1] = 0.5;
-			OSCtoggle[0] = 0;  // Normal mode
+			OSCtoggle[0] = 0; // Normal mode
 			mode = 0;
 			modifing_control_parameters = false;
 		}
 
-		if (OSCmove_mode) {
-			//Serial.print("M ");
-			//Serial.print(OSCmove_speed);
-			//Serial.print(" ");
-			//Serial.print(OSCmove_steps1);
-			//Serial.print(",");
-			//Serial.println(OSCmove_steps2);
+		if (OSCmove_mode)
+		{
+#ifdef DEBUG_OSC
+			DB_PRINTLN("OSCmove_mode positionControlMode = true");
+			DB_PRINT("M ");
+			DB_PRINT(OSCmove_speed);
+			DB_PRINT(" ");
+			DB_PRINT(OSCmove_steps1);
+			DB_PRINT(",");
+			DB_PRINTLN(OSCmove_steps2);
+#endif			
 			positionControlMode = true;
 			OSCmove_mode = false;
 			target_steps1 = steps1 + OSCmove_steps1;
 			target_steps2 = steps2 + OSCmove_steps2;
-		} else {
+		}
+		else
+		{
 			positionControlMode = false;
 			throttle = (OSCfader[0] - 0.5) * max_throttle;
 			// We add some exponential on steering to smooth the center band
 			steering = OSCfader[1] - 0.5;
 			if (steering > 0)
-				steering = (steering * steering + 0.5 * steering)
-						* max_steering;
+				steering = (steering * steering + 0.5 * steering) * max_steering;
 			else
-				steering = (-steering * steering + 0.5 * steering)
-						* max_steering;
+				steering = (-steering * steering + 0.5 * steering) * max_steering;
+#ifdef DEBUG_OSC
+			DB_PRINT("OSC throttle: ");
+			DB_PRINTLN(throttle);
+			DB_PRINT("OSC steering: ");
+			DB_PRINTLN(steering);
+#endif			
 		}
 
-		if ((mode == 0) && (OSCtoggle[0])) {
+		if ((mode == 0) && (OSCtoggle[0]))
+		{
 			// Change to PRO mode
+			DB_PRINTLN("OSC Change to PRO mode");
 			max_throttle = MAX_THROTTLE_PRO;
 			max_steering = MAX_STEERING_PRO;
 			max_target_angle = MAX_TARGET_ANGLE_PRO;
 			mode = 1;
 		}
-		if ((mode == 1) && (OSCtoggle[0] == 0)) {
+		if ((mode == 1) && (OSCtoggle[0] == 0))
+		{
 			// Change to NORMAL mode
+			DB_PRINTLN("OSC Change to NORMAL mode");
 			max_throttle = MAX_THROTTLE;
 			max_steering = MAX_STEERING;
 			max_target_angle = MAX_TARGET_ANGLE;
 			mode = 0;
 		}
-	} else if (OSCpage == 2) { // OSC page 2
-		if (!modifing_control_parameters) {
+	}
+	else if (OSCpage == 2)
+	{ // OSC page 2
+		if (!modifing_control_parameters)
+		{
 			for (uint8_t i = 0; i < 4; i++)
 				OSCfader[i] = 0.5;
 			OSCtoggle[0] = 0;
 
 			modifing_control_parameters = true;
-			OSC_MsgSend("$P2", 4);
+			char buf[] = "$P2";
+			OSC_MsgSend(buf, 4);
 		}
 		// User could adjust KP, KD, KP_THROTTLE and KI_THROTTLE (fadder1,2,3,4)
 		// Now we need to adjust all the parameters all the times because we dont know what parameter has been moved
@@ -149,48 +181,55 @@ void processOSCMsg() {
 				int(Ki_thr_user * 1000));
 		OSC_MsgSend(auxS, 50);
 
-#if DEBUG>0
-		Serial.print("Par: ");
-		Serial.print(Kp_user);
-		Serial.print(" ");
-		Serial.print(Kd_user);
-		Serial.print(" ");
-		Serial.print(Kp_thr_user);
-		Serial.print(" ");
-		Serial.println(Ki_thr_user);
+#ifdef DEBUG_OSC
+		DB_PRINTLN("OSC modifing control parameters");
+		DB_PRINT("Par: ");
+		DB_PRINT(Kp_user);
+		DB_PRINT(" ");
+		DB_PRINT(Kd_user);
+		DB_PRINT(" ");
+		DB_PRINT(Kp_thr_user);
+		DB_PRINT(" ");
+		DB_PRINTLN(Ki_thr_user);
 #endif
 
 		// Calibration mode??
-		if (OSCpush[2] == 1) {
-			Serial.print("Calibration MODE ");
+		if (OSCpush[2] == 1)
+		{
+			DB_PRINT("OSC Calibration MODE ");
 			angle_offset = angle_adjusted_filtered;
-			Serial.println(angle_offset);
+			DB_PRINTLN(angle_offset);
 		}
 
 		// Kill robot => Sleep
-		while (OSCtoggle[0] == 1) {
-			//Reset external parameters
+		while (OSCtoggle[0] == 1)
+		{
+			DB_PRINTLN("OSC Sleep");
+			// Reset external parameters
 			PID_errorSum = 0;
 			timer_old = millis();
 			setMotorSpeedM1(0);
 			setMotorSpeedM2(0);
-			digitalWrite(PIN_ENABLE_MOTORS, HIGH);  // Disable motors
+			digitalWrite(PIN_ENABLE_MOTORS, HIGH); // Disable motors
 			OSC_MsgRead();
 		}
 	}
 }
 
-void loop() {
+void loop()
+{
 	OSC_MsgRead();
 
-	if (OSCnewMessage) {
+	if (OSCnewMessage)
+	{
 		OSCnewMessage = 0;
 		processOSCMsg();
 	}
 
 	timer_value = micros();
 
-	if (MPU6050_newData()) {
+	if (MPU6050_newData())
+	{
 		MPU6050_read_3axis();
 
 		loop_counter++;
@@ -203,41 +242,36 @@ void loop() {
 		float MPU_sensor_angle = MPU6050_getAngle(dt);
 		angle_adjusted = MPU_sensor_angle + angle_offset;
 		if ((MPU_sensor_angle > -15) && (MPU_sensor_angle < 15))
-			angle_adjusted_filtered = angle_adjusted_filtered * 0.99
-					+ MPU_sensor_angle * 0.01;
+			angle_adjusted_filtered = angle_adjusted_filtered * 0.99 + MPU_sensor_angle * 0.01;
 
-#if DEBUG==1
-		Serial.print(dt);
-		Serial.print(" ");
-		Serial.print(angle_offset);
-		Serial.print(" ");
-		Serial.print(angle_adjusted);
-		Serial.print(",");
-		Serial.println(angle_adjusted_filtered);
+#ifdef DEBUG_IMU
+		DB_PRINT(dt);
+		DB_PRINT(" ");
+		DB_PRINT(angle_offset);
+		DB_PRINT(" ");
+		DB_PRINT(angle_adjusted);
+		DB_PRINT(",");
+		DB_PRINTLN(angle_adjusted_filtered);
 #endif
-		//Serial.print("\t");
-
 		// We calculate the estimated robot speed:
 		// Estimated_Speed = angular_velocity_of_stepper_motors(combined) - angular_velocity_of_robot(angle measured by IMU)
 		actual_robot_speed = (speed_M1 + speed_M2) / 2; // Positive: forward
 
 		int16_t angular_velocity = (angle_adjusted - angle_adjusted_Old) * 25.0; // 25 is an empirical extracted factor to adjust for real units
 		int16_t estimated_speed = -actual_robot_speed + angular_velocity;
-		estimated_speed_filtered = estimated_speed_filtered * 0.9
-				+ (float) estimated_speed * 0.1; // low pass filter on estimated speed
+		estimated_speed_filtered = estimated_speed_filtered * 0.9 + (float)estimated_speed * 0.1; // low pass filter on estimated speed
 
-#if DEBUG==2
-				Serial.print(angle_adjusted);
-				Serial.print(" ");
-				Serial.println(estimated_speed_filtered);
+#ifdef DEBUG_ESTIMATED_SPEED
+		DB_PRINT(angle_adjusted);
+		DB_PRINT(" ");
+		DB_PRINTLN(estimated_speed_filtered);
 #endif
 
-		if (positionControlMode) {
+		if (positionControlMode)
+		{
 			// POSITION CONTROL. INPUT: Target steps for each motor. Output: motors speed
-			motor1_control = positionPDControl(steps1, target_steps1,
-					Kp_position, Kd_position, speed_M1);
-			motor2_control = positionPDControl(steps2, target_steps2,
-					Kp_position, Kd_position, speed_M2);
+			motor1_control = positionPDControl(steps1, target_steps1, Kp_position, Kd_position, speed_M1);
+			motor2_control = positionPDControl(steps2, target_steps2, Kp_position, Kd_position, speed_M2);
 
 			// Convert from motor position control to throttle / steering commands
 			throttle = (motor1_control + motor2_control) / 2;
@@ -248,26 +282,22 @@ void loop() {
 
 		// ROBOT SPEED CONTROL: This is a PI controller.
 		//    input:user throttle(robot speed), variable: estimated robot speed, output: target robot angle to get the desired speed
-		target_angle = speedPIControl(dt, estimated_speed_filtered, throttle,
-				Kp_thr, Ki_thr);
-		target_angle = constrain(target_angle, -max_target_angle,
-				max_target_angle); // limited output
+		target_angle = speedPIControl(dt, estimated_speed_filtered, throttle, Kp_thr, Ki_thr);
+		target_angle = constrain(target_angle, -max_target_angle, max_target_angle); // limited output
 
-#if DEBUG==3
-		Serial.print(angle_adjusted);
-		Serial.print(" ");
-		Serial.print(estimated_speed_filtered);
-		Serial.print(" ");
-		Serial.println(target_angle);
+#ifdef DEBUG_SPEED
+		DB_PRINT(angle_adjusted);
+		DB_PRINT(" ");
+		DB_PRINT(estimated_speed_filtered);
+		DB_PRINT(" ");
+		DB_PRINTLN(target_angle);
 #endif
 
 		// Stability control (100Hz loop): This is a PD controller.
 		//    input: robot target angle(from SPEED CONTROL), variable: robot angle, output: Motor speed
 		//    We integrate the output (sumatory), so the output is really the motor acceleration, not motor speed.
-		control_output += stabilityPDControl(dt, angle_adjusted, target_angle,
-				Kp, Kd);
-		control_output = constrain(control_output, -MAX_CONTROL_OUTPUT,
-				MAX_CONTROL_OUTPUT); // Limit max output from control
+		control_output += stabilityPDControl(dt, angle_adjusted, target_angle, Kp, Kd);
+		control_output = constrain(control_output, -MAX_CONTROL_OUTPUT, MAX_CONTROL_OUTPUT); // Limit max output from control
 
 		// The steering part from the user is injected directly to the output
 		motor1 = control_output + steering;
@@ -278,24 +308,25 @@ void loop() {
 		motor2 = constrain(motor2, -MAX_CONTROL_OUTPUT, MAX_CONTROL_OUTPUT);
 
 		int angle_ready;
-		if (OSCpush[0])     // If we press the SERVO button we start to move
+		if (OSCpush[0]) // If we press the SERVO button we start to move
 			angle_ready = 82;
 		else
-			angle_ready = 74;  // Default angle
+			angle_ready = 74;												   // Default angle
 		if ((angle_adjusted < angle_ready) && (angle_adjusted > -angle_ready)) // Is robot ready (upright?)
-				{
+		{
 			// NORMAL MODE
-			digitalWrite(PIN_ENABLE_MOTORS, LOW);  // Motors enable
+			digitalWrite(PIN_ENABLE_MOTORS, LOW); // Motors enable
 			// NOW we send the commands to the motors
 			setMotorSpeedM1(motor1);
 			setMotorSpeedM2(motor2);
-		} else   // Robot not ready (flat), angle > angle_ready => ROBOT OFF
+		}
+		else // Robot not ready (flat), angle > angle_ready => ROBOT OFF
 		{
-			digitalWrite(PIN_ENABLE_MOTORS, HIGH);  // Disable motors
+			digitalWrite(PIN_ENABLE_MOTORS, HIGH); // Disable motors
 			setMotorSpeedM1(0);
 			setMotorSpeedM2(0);
-			PID_errorSum = 0;  // Reset PID I term
-			Kp = KP_RAISEUP;   // CONTROL GAINS FOR RAISE UP
+			PID_errorSum = 0; // Reset PID I term
+			Kp = KP_RAISEUP;  // CONTROL GAINS FOR RAISE UP
 			Kd = KD_RAISEUP;
 			Kp_thr = KP_THROTTLE_RAISEUP;
 			Ki_thr = KI_THROTTLE_RAISEUP;
@@ -308,27 +339,30 @@ void loop() {
 			steering = 0;
 		}
 		// Push1 Move servo arm
-		if (OSCpush[0])  // Move arm
+		if (OSCpush[0]) // Move arm
 		{
 			if (angle_adjusted > -40)
 				ledcWrite(6, SERVO_MIN_PULSEWIDTH);
 			else
 				ledcWrite(6, SERVO_MAX_PULSEWIDTH);
-		} else
+		}
+		else
 			ledcWrite(6, SERVO_AUX_NEUTRO);
 
 		// Servo2
-		//ledcWrite(6, SERVO2_NEUTRO + (OSCfader[2] - 0.5) * SERVO2_RANGE);
+		// ledcWrite(6, SERVO2_NEUTRO + (OSCfader[2] - 0.5) * SERVO2_RANGE);
 
 		// Normal condition?
-		if ((angle_adjusted < 56) && (angle_adjusted > -56)) {
-			Kp = Kp_user;            // Default user control gains
+		if ((angle_adjusted < 56) && (angle_adjusted > -56))
+		{
+			Kp = Kp_user; // Default user control gains
 			Kd = Kd_user;
 			Kp_thr = Kp_thr_user;
 			Ki_thr = Ki_thr_user;
-		} else // We are in the raise up procedure => we use special control parameters
+		}
+		else // We are in the raise up procedure => we use special control parameters
 		{
-			Kp = KP_RAISEUP;         // CONTROL GAINS FOR RAISE UP
+			Kp = KP_RAISEUP; // CONTROL GAINS FOR RAISE UP
 			Kd = KD_RAISEUP;
 			Kp_thr = KP_THROTTLE_RAISEUP;
 			Ki_thr = KI_THROTTLE_RAISEUP;
@@ -337,37 +371,38 @@ void loop() {
 	} // End of new IMU data
 
 	// Medium loop 7.5Hz
-	if (loop_counter >= 15) {
+	if (loop_counter >= 15)
+	{
 		loop_counter = 0;
 		// Telemetry here?
-#if TELEMETRY_ANGLE==1
+#if TELEMETRY_ANGLE == 1
 		char auxS[25];
 		int ang_out = constrain(int(angle_adjusted * 10), -900, 900);
 		sprintf(auxS, "$tA,%+04d", ang_out);
 		OSC_MsgSend(auxS, 25);
 #endif
-#if TELEMETRY_DEBUG==1
+#if TELEMETRY_DEBUG == 1
 		char auxS[50];
 		sprintf(auxS, "$tD,%d,%d,%ld", int(angle_adjusted * 10), int(estimated_speed_filtered), steps1);
 		OSC_MsgSend(auxS, 50);
 #endif
-
 	} // End of medium loop
 	else if (slow_loop_counter >= 100) // 1Hz
-			{
+	{
 		slow_loop_counter = 0;
 		// Read  status
-#if TELEMETRY_BATTERY==1
+#if TELEMETRY_BATTERY == 1
 		BatteryValue = (BatteryValue + BROBOT_readBattery(false)) / 2;
 		sendBattery_counter++;
-		if (sendBattery_counter >= 3) { //Every 3 seconds we send a message
+		if (sendBattery_counter >= 3)
+		{ // Every 3 seconds we send a message
 			sendBattery_counter = 0;
-			Serial.print("B");
-			Serial.println(BatteryValue);
+			DB_PRINT("B");
+			DB_PRINTLN(BatteryValue);
 			char auxS[25];
 			sprintf(auxS, "$tB,%04d", BatteryValue);
 			OSC_MsgSend(auxS, 25);
 		}
 #endif
-	}  // End of slow loop
+	} // End of slow loop
 }
--- src/MPU6050.cpp
+++ src/MPU6050.cpp
@@ -21,37 +21,38 @@
 
 // MPU6050 Register map
 
-
 // Global MPU6050 IMU variables
 accel_t_gyro_union accel_t_gyro;
-float x_gyro_value;   // in deg/seg units
+float x_gyro_value; // in deg/seg units
 float x_gyro_offset = 0.0;
-float accel_angle;  // in degree units
+float accel_angle; // in degree units
 float angle;
 
 // Util function to swap byte values
 uint8_t swap;
-#define SWAP(x,y) swap = x; x = y; y = swap
-
+#define SWAP(x, y) \
+  swap = x;        \
+  x = y;           \
+  y = swap
 
 // This function implements a complementary filter to fusion gyro and accel info
 float MPU6050_getAngle(float dt)
 {
   accel_angle = atan2f((float)accel_t_gyro.value.y_accel, (float)accel_t_gyro.value.z_accel) * RAD2GRAD;
-  x_gyro_value = (accel_t_gyro.value.x_gyro - x_gyro_offset) / 65.5;  // Accel scale at 500deg/seg  => 65.5 LSB/deg/s
+  x_gyro_value = (accel_t_gyro.value.x_gyro - x_gyro_offset) / 65.5; // Accel scale at 500deg/seg  => 65.5 LSB/deg/s
 
   // Complementary filter
   // We integrate the gyro rate value to obtain the angle in the short term and we take the accelerometer angle with a low pass filter in the long term...
-  angle = 0.99 * (angle + x_gyro_value * dt) + 0.01 * accel_angle;  // Time constant = 0.99*0.01(100hz)/(1-0.99) = 0.99, around 1 sec.
+  angle = 0.99 * (angle + x_gyro_value * dt) + 0.01 * accel_angle; // Time constant = 0.99*0.01(100hz)/(1-0.99) = 0.99, around 1 sec.
 
   // Gyro bias correction
   // We supose that the long term mean of the gyro_value should tend to zero (gyro_offset). This means that the robot is not continuosly rotating.
   int16_t correction = constrain(accel_t_gyro.value.x_gyro, x_gyro_offset - 10, x_gyro_offset + 10); // limit corrections...
-  x_gyro_offset = x_gyro_offset * 0.9995 + correction * 0.0005; // Time constant of this correction is around 20 sec.
+  x_gyro_offset = x_gyro_offset * 0.9995 + correction * 0.0005;                                      // Time constant of this correction is around 20 sec.
 
-  //Serial.print(angle);
-  //Serial.print(" ");
-  //Serial.println(x_gyro_offset);
+  // Serial.print(angle);
+  // Serial.print(" ");
+  // Serial.println(x_gyro_offset);
 
   return angle;
 }
@@ -66,7 +67,8 @@ void MPU6050_calibrate()
   bool gyro_cal_ok = false;
 
   delay(500);
-  while (!gyro_cal_ok){
+  while (!gyro_cal_ok)
+  {
     Serial.println("Gyro calibration... DONT MOVE!");
     // we take 100 measurements in 4 seconds
     for (i = 0; i < 100; i++)
@@ -99,6 +101,7 @@ void MPU6050_calibrate()
 
 void MPU6050_setup()
 {
+#ifdef DEBUG_MPU
   int error;
   uint8_t c;
 
@@ -107,13 +110,14 @@ void MPU6050_setup()
   Serial.print(c, HEX);
   Serial.print(", error = ");
   Serial.println(error, DEC);
+#endif  
 
   // RESET chip
   MPU6050_write_reg(MPU6050_PWR_MGMT_1, bit(MPU6050_DEVICE_RESET));
   delay(125);
   // Clear the 'sleep' bit to start the sensor and select clock source
-  MPU6050_write_reg (MPU6050_PWR_MGMT_1, 0x01);
-  //MPU6050_write_reg(MPU6050_PWR_MGMT_1,MPU6050_CLKSEL_Z);
+  MPU6050_write_reg(MPU6050_PWR_MGMT_1, 0x01);
+  // MPU6050_write_reg(MPU6050_PWR_MGMT_1,MPU6050_CLKSEL_Z);
 
   // Config Gyro scale (500deg/seg)
   MPU6050_write_reg(MPU6050_GYRO_CONFIG, MPU6050_FS_SEL_500);
@@ -122,15 +126,15 @@ void MPU6050_setup()
   // Config Digital Low Pass Filter 10Hz
   MPU6050_write_reg(MPU6050_CONFIG, MPU6050_DLPF_10HZ);
   // Set Sample Rate to 100Hz
-  MPU6050_write_reg(MPU6050_SMPLRT_DIV, 9);  // 100Hz : Sample Rate = 1000 / (1 + SMPLRT_DIV) Hz
+  MPU6050_write_reg(MPU6050_SMPLRT_DIV, 9); // 100Hz : Sample Rate = 1000 / (1 + SMPLRT_DIV) Hz
   // Data ready interrupt enable
   MPU6050_write_reg(MPU6050_INT_ENABLE, MPU6050_DATA_RDY_EN);
   // Clear the 'sleep' bit to start the sensor (and select clock source).
-  MPU6050_write_reg (MPU6050_PWR_MGMT_1, 0x01);
+  MPU6050_write_reg(MPU6050_PWR_MGMT_1, 0x01);
 
   // Clear the 'sleep' bit to start the sensor.
-  //MPU6050_write_reg(MPU6050_PWR_MGMT_1,MPU6050_CLKSEL_Z);
-  //MPU6050_write_reg (MPU6050_PWR_MGMT_1, 0);
+  // MPU6050_write_reg(MPU6050_PWR_MGMT_1,MPU6050_CLKSEL_Z);
+  // MPU6050_write_reg (MPU6050_PWR_MGMT_1, 0);
 }
 
 void MPU6050_read_3axis()
@@ -138,19 +142,21 @@ void MPU6050_read_3axis()
   int error;
 
   // read 14 bytes (gyros, temp and accels)
-  error = MPU6050_read (MPU6050_ACCEL_XOUT_H, (uint8_t *) &accel_t_gyro, sizeof(accel_t_gyro));
-  if (error != 0) {
-    //Serial.print("MPU6050 Error:");
-    //Serial.println(error);
+  error = MPU6050_read(MPU6050_ACCEL_XOUT_H, (uint8_t *)&accel_t_gyro, sizeof(accel_t_gyro));
+  if (error != 0)
+  {
+    Serial.print("MPU6050 Error:");
+    Serial.println(error);
+    return;
   }
   // swap bytes
-  SWAP (accel_t_gyro.reg.x_accel_h, accel_t_gyro.reg.x_accel_l);
-  SWAP (accel_t_gyro.reg.y_accel_h, accel_t_gyro.reg.y_accel_l);
-  SWAP (accel_t_gyro.reg.z_accel_h, accel_t_gyro.reg.z_accel_l);
-  SWAP (accel_t_gyro.reg.t_h, accel_t_gyro.reg.t_l);
-  SWAP (accel_t_gyro.reg.x_gyro_h, accel_t_gyro.reg.x_gyro_l);
-  SWAP (accel_t_gyro.reg.y_gyro_h, accel_t_gyro.reg.y_gyro_l);
-  SWAP (accel_t_gyro.reg.z_gyro_h, accel_t_gyro.reg.z_gyro_l);
+  SWAP(accel_t_gyro.reg.x_accel_h, accel_t_gyro.reg.x_accel_l);
+  SWAP(accel_t_gyro.reg.y_accel_h, accel_t_gyro.reg.y_accel_l);
+  SWAP(accel_t_gyro.reg.z_accel_h, accel_t_gyro.reg.z_accel_l);
+  SWAP(accel_t_gyro.reg.t_h, accel_t_gyro.reg.t_l);
+  SWAP(accel_t_gyro.reg.x_gyro_h, accel_t_gyro.reg.x_gyro_l);
+  SWAP(accel_t_gyro.reg.y_gyro_h, accel_t_gyro.reg.y_gyro_l);
+  SWAP(accel_t_gyro.reg.z_gyro_h, accel_t_gyro.reg.z_gyro_l);
 
   /*
     // Print the raw acceleration values
@@ -179,26 +185,32 @@ void MPU6050_read_1axis()
   int error;
 
   // read X accel
-  error = MPU6050_read(MPU6050_ACCEL_XOUT_H, (uint8_t *) &accel_t_gyro.reg.x_accel_h, 6);
-  if (error != 0) {
-    //Serial.print("MPU6050 Error:");
-    //Serial.println(error);
+  error = MPU6050_read(MPU6050_ACCEL_XOUT_H, (uint8_t *)&accel_t_gyro.reg.x_accel_h, 6);
+  if (error != 0)
+  {
+    Serial.print("MPU6050 Error:");
+    Serial.println(error);
+    return;
   }
   // read X gyro
-  error = MPU6050_read(MPU6050_GYRO_XOUT_H, (uint8_t *) &accel_t_gyro.reg.x_gyro_h, 2);
-  if (error != 0) {
-    //Serial.print("MPU6050 Error:");
-    //Serial.println(error);
+  error = MPU6050_read(MPU6050_GYRO_XOUT_H, (uint8_t *)&accel_t_gyro.reg.x_gyro_h, 2);
+  if (error != 0)
+  {
+    Serial.print("MPU6050 Error:");
+    Serial.println(error);
+    return;
   }
-  SWAP (accel_t_gyro.reg.x_accel_h, accel_t_gyro.reg.y_accel_l);
-  SWAP (accel_t_gyro.reg.z_accel_h, accel_t_gyro.reg.z_accel_l);
-  SWAP (accel_t_gyro.reg.x_gyro_h, accel_t_gyro.reg.x_gyro_l);
+  SWAP(accel_t_gyro.reg.x_accel_h, accel_t_gyro.reg.y_accel_l);
+  SWAP(accel_t_gyro.reg.z_accel_h, accel_t_gyro.reg.z_accel_l);
+  SWAP(accel_t_gyro.reg.x_gyro_h, accel_t_gyro.reg.x_gyro_l);
 
+/*
   // Print values
   Serial.print("axis:");
   Serial.print(accel_t_gyro.value.y_accel, DEC);
   Serial.print(",");
   Serial.println(accel_t_gyro.value.x_gyro, DEC);
+  */
 }
 
 // return true on new data available
@@ -208,10 +220,13 @@ bool MPU6050_newData()
   int error;
 
   error = MPU6050_read(MPU6050_INT_STATUS, &status, 1);
-  if (error != 0) {
-    //Serial.print("MPU6050 Error:");
-    //Serial.println(error);
+  if (error != 0)
+  {
+    Serial.print("MPU6050 Error:");
+    Serial.println(error);
+    return false;
   }
+
   if (status & (0b00000001)) // Data ready?
     return true;
   else
@@ -228,23 +243,22 @@ int MPU6050_read(int start, uint8_t *buffer, int size)
   if (n != 1)
     return (-10);
 
-  n = Wire.endTransmission(false);    // hold the I2C-bus
+  n = Wire.endTransmission(true); // hold the I2C-bus
   if (n != 0)
     return (n);
 
   // Third parameter is true: relase I2C-bus after data is read.
-  Wire.requestFrom((uint8_t) MPU6050_I2C_ADDRESS, (size_t) size, (bool) true);
+  Wire.requestFrom((uint16_t)MPU6050_I2C_ADDRESS, (uint8_t)size, (bool)true);
   i = 0;
   while (Wire.available() && i < size)
   {
     buffer[i++] = Wire.read();
   }
-  if ( i != size)
+  if (i != size)
     return (-11);
 
-  return (0);  // return : no error
+  return (0); // return : no error
 }
-
 
 // MPU6050_write n bytes
 int MPU6050_write(int start, const uint8_t *pData, int size)
@@ -252,11 +266,11 @@ int MPU6050_write(int start, const uint8_t *pData, int size)
   int n, error;
 
   Wire.beginTransmission(MPU6050_I2C_ADDRESS);
-  n = Wire.write(start);        // write the start address
+  n = Wire.write(start); // write the start address
   if (n != 1)
     return (-20);
 
-  n = Wire.write(pData, size);  // write data bytes
+  n = Wire.write(pData, size); // write data bytes
   if (n != size)
     return (-21);
 
@@ -264,7 +278,7 @@ int MPU6050_write(int start, const uint8_t *pData, int size)
   if (error != 0)
     return (error);
 
-  return (0);         // return : no error
+  return (0); // return : no error
 }
 
 // --------------------------------------------------------
